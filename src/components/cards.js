05e98328820a9209773f885cb0f47c3595a2724f
Maria Paula Miceli <mariapaulamiceli@gmail.com>
2018-05-22 23:40:37 -0300

    Filtro por tecnologia OK

--- src/components/cards.js
+++ src/components/cards.js
@@ -2,45 +2,36 @@ import React, { Component } from 'react';
 import '../cards.css';
 import {textos} from '../textos.json';
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
-import Yava from './javascript.js';
+import Card from './card.js';
 
+class Cards extends Component {
 
-
-class Cards extends Component{
-  constructor(){
+  constructor() {
     super();
-    this.state={
-      textos
+
+    this.state = {
+      filtrarPor: "Frontend" // "Backend", "Frontend", null
     }
   }
+
   render(){
-
-    const dataCard = this.state.textos.map(function(data, i ){
-          return(
-          <div className="cont col-md-4" >
-          <div>
-            <img className="card-img-top" src={data.cardImageUrl} alt="Card image cap"></img>
-            <div className="card-body">
-            <Link to={"/post/"+data.cardTitle}><p>{data.cardDescription}</p></Link>
-            </div>
-            </div>
-          </div>
-        );
-
+    let filtrarPor = this.state.filtrarPor;
+    const cards = textos.map(function(data, i ){
+      return (
+        <Card info={data} filtrarPor={filtrarPor} />
+      );
     })
 
     return(
       <div className="caja">
         <div className="cards">
           <div className="row">
-            {dataCard}
+            {cards}
           </div>
         </div>
       </div>
     );
   }
 }
-
-
 
 export default Cards;
